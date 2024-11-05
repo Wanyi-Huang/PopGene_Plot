23817773c737abab77c691b80d9f98288979a59c
Wanyi-Huang <38121375+Wanyi-Huang@users.noreply.github.com>
2024-11-05 15:07:12 -0600

    Update dxy_line.R

--- R/dxy_line.R
+++ R/dxy_line.R
@@ -1,7 +1,32 @@
+#There are 4 populations/isolates named as 1, 2, 3, and 4. We will plot the dxy values between the 2 and other ones. 
+
+#Combine the files with the dxy values of each pair of populations/isolates
+ID <- 2
+for (i in 1:length(ID)){
+  pop1 <- ID[i]
+  ID2 <- as.character(c(1:4))
+  ID2 <- ID2[ID2 != pop1]
+  df<-read.csv("Pop2_1.csv", header = T)
+  df<-data.frame(df[,1:5])
+  for (j in 1:length(ID2)){
+    pop2 <- ID2[j]
+    dxy <- read.csv(paste("Pop",pop1,"_",pop2,".csv", sep=""), header = T)
+    df <- cbind(df,pop2=dxy[,8])
+    names(df)[names(df)=="pop2"] <- paste(pop2,sep="")
+    for (k in 1:8){
+      ch <- df[df[,1] == k,]
+      write.table(ch,paste(pop1,"_Chr", k ,".txt",sep=""),sep="\t", row.names = F, col.names = T)
+    }
+  }
+  write.table(df,paste(pop1,".txt",sep=""),sep="\t", row.names = F, col.names = T)
+}
+
+#Library packages:
 library(ggplot2)
 library(gridExtra)
-PopA<-"4"
-PopB<-c(5,3,8)
+PopA<-"2"
+PopB<-c(1,3,4)
+
 #Making combined plot
 for (j in 1:8){
   dxy<-read.table(paste(PopA,"_Chr",j,".txt",sep = ""),header = T)
@@ -34,12 +59,12 @@ for (j in 1:8){
                  axis.text.x = element_text(size=rel(1.2),angle=90,hjust = 1,color="black"),
                  plot.margin=unit(x=c(top.mar,right.mar,bottom.mar,left.mar),units="inches"))
   assign(paste("Fig",j,sep = ""),ggplot(Fipop, aes(x=factor(loc), y=dxy, group=factor(pop), color=factor(pop))) + geom_line(aes(color=factor(pop)),linewidth=1) + scale_y_continuous(limits = c(0,1),breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1),label = c("0.0","0.2","0.4","0.6","0.8","1.0")) + mytheme)
-  }
+}
 grid.arrange(arrangeGrob(Fig1,Fig2,Fig3,Fig4,nrow=4),arrangeGrob(Fig5,Fig6,Fig7,Fig8,nrow=4),ncol=2,widths=c(4,4))
 
 #Makeing single plot
-PopA<-"4"
-PopB<-"6"
+PopA<-"2"
+PopB<-"1"
 
 for (j in 1:8){
   dxy<-read.table(paste(PopA,"_Chr",j,".txt",sep = ""),header = T)
