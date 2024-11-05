ff6fdaaff8b6e89a7c065bf1fdab8fe84a6429ad
Wanyi-Huang <38121375+Wanyi-Huang@users.noreply.github.com>
2024-11-05 15:35:14 -0600

    Update Window_fd.R

--- R/Window_fd.R
+++ R/Window_fd.R
@@ -1,22 +1,29 @@
+#Library packages:
 library(ggplot2)
-#6789.csv is the result from Genomics_general (https://github.com/simonhmartin/genomics_general)
-df <-read.csv("6789.csv",header = T)
+
+#Window_fd.csv is the result from Genomics_general (https://github.com/simonhmartin/genomics_general)
+#Read data:
+df <-read.csv("Window_fd.csv",header = T)
 data <-df[,c(1:3,10)]
 
+#Keep the data with fd values.
 rowN<-which(data$fd > 0 & data$fd < 1 | data$fd == 1 | data$fd == 0 )
 rowN1 <- c(1:length(data$fd))
 rowN2 <- rowN1[! rowN1%in%rowN]
-
 for (i in 1:length(rowN2)){
-    O <- rowN2[i]
-    data[O,"fd"]<- 0
+  O <- rowN2[i]
+  data[O,"fd"]<- 0
 }
+
+#Create a new data frame to plot the dxy. The 10000 represents the length of the sliding window.
 data$pos<-seq(1, length(data$fd)*10000, by=10000)
 
+#Create a fd plot:
 p1<-ggplot(data, aes(pos, fd))+geom_point(aes(y=fd,color=scaffold))
 
 p1<-p1+labs(y="pi",x=NULL)
 
+#Format the plot:
 top.mar=0.2
 right.mar=0.2
 bottom.mar=0.2
